608ef6978108182091d7eef19083c0ae65e0a62d
prgao <peiran_gao@me.com>
2014-10-06 17:47:01 -0700

    added methods to sample the process as well as its spectrum

--- armp.jl
+++ armp.jl
@@ -1,15 +1,70 @@
 module ARMP
 
-export ARMPModel, spectrum, ub, lb
+export ARMPModel, rand, randSpec, spec, ub, lb
 
 using Optim
 
-global epsilon = 1e-9
+global const epsilon = 1e-9
 
 # A high dimensional autoregressive model with shape parameter c and decay phi
 immutable ARMPModel
+	p::Integer
+	n::Integer
 	c::Float64
 	phi::Float64
+
+	function ARMPModel(p::Integer, n::Integer, phi::Float64)
+		c = (p / n)::Float64
+		if !(0 < c && 0 < phi < 1)
+			error("c or phi out of bound")
+		else
+			return new(p, n, c, phi)
+		end
+	end
+
+	function ARMPModel(cp::Float64, phi::Float64)
+		if !(0 < cp && 0 < phi < 1)
+			error("c or phi out of bound")
+		end
+		if cp < 1
+			p = 1000::Int64
+			n = int(p / cp)
+		else
+			n = 1000::Int64
+			p = int(n * cp)
+		end
+		c = p / n
+		if abs(c - cp) > 1e-6
+			warn("actual c set to ", c)
+		end
+		return new(p, n, c, phi)
+	end
+end
+
+# sample a model
+function rand(model::ARMPModel)
+	let p = model.p, n = model.n, phi = model.phi, c = model.c
+		x = zeros(p, n)
+		P = sqrt(1.0 - phi^2) * eye(p)
+		x[:, 1] = P * randn(p);
+		for t in 2:n
+			x[:, t] = phi * x[:, t - 1] + randn(p)
+		end
+		return x
+	end
+end
+
+# sample the spectrum of a model nTrial times
+function randSpec(model::ARMPModel, nTrial::Integer)
+	let p = model.p, n = model.n, phi = model.phi, c = model.c
+		D = Array(Float64, p * nTrial)
+		for kTrial in 1:nTrial
+			x = rand(model)
+			d, _ = eig(x * x' / n)
+			D[(kTrial - 1) * p + 1:kTrial * p] = d
+		end
+		return D
+	end
 end
 
 function zFunc(model::ARMPModel)
@@ -53,11 +108,11 @@ function stieltjes(model::ARMPModel)
 end
 
 # Returns a spectrum function for the given model
-function spectrum(model::ARMPModel)
+function spec(model::ARMPModel)
 	global epsilon
 	s = stieltjes(model)
 
-	return x -> real((s(x + epsilon * 1im) - s(x - epsilon * 1im)) / 2.0im / pi)
+	return x -> abs(real((s(x + epsilon * 1im) - s(x - epsilon * 1im)) / 2.0im / pi))
 end
 
 end
